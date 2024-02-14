dc714241f4bc75a82641467ecbe01de79b4ab89e
Gio <86983840+gioaprilino@users.noreply.github.com>
2024-02-15 02:16:25 +0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy zip here
-gh auth login
+github auth login
 filename=$(ls *.zip)
 
 # Create a tag and release using the filename (without .zip extension)
@@ -11,5 +11,5 @@ git tag -a "$version" -m "Release $version"
 
 git push origin "$version"
 
-gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
+github release create "$version" "$filename" -t "Release $version" -n "Release notes"
 
